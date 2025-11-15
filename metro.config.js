2d8c4480c52137e28f887831e20c0eb9125b73da
yocfking-alt <yocfking@gmail.com>
2025-11-15 22:19:44 +0100

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.transformer.babelTransformerPath = require.resolve('react-native-web/babel transformer');
+
+config.resolver.assetExts.push(
+  'db'
+);
+
+module.exports = config;
